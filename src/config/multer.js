27c82884157747a3f96b3874c39d155c50937a1a
EmersonLaranja <emersonlaranja@gmail.com>
2021-01-19 18:26:17 -0300

    settings about file upload with multer

---
+++ src/config/multer.js
@@ -0,0 +1,18 @@
+// settings about upload images
+import multer from 'multer';
+import crypto from 'crypto';
+import { extname, resolve } from 'path';
+
+export default {
+  storage: multer.diskStorage({
+    // how multer will keep our images
+    destination: resolve(__dirname, '..', '..', 'temp', 'uploads'),
+    filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, res) => {
+        if (err) return cb(err);
+
+        return cb(null, res.toString('hex') + extname(file.originalname));
+      });
+    },
+  }),
+};
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,21 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
 routes.use(authMiddleware); // it works for each route below
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), (req, res) =>
+  res.json({ ok: true })
+);
 
 export default routes;

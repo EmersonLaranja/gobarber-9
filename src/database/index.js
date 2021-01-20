a36d8b7b68e3e0b13f2894a6ff0e6e398fd1fd8b
EmersonLaranja <emersonlaranja@gmail.com>
2021-01-20 00:51:14 -0300

    setting mongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,8 @@
 // Connect the postgres db and load the models, so they'll be known in any part of the app
 import Sequelize from 'sequelize';
-import databaseConfig from '../config/database';
+import mongoose from 'mongoose';
 
+import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
@@ -11,6 +12,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -21,6 +23,17 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models)
       );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true,
+        useFindAndModify: true,
+        useUnifiedTopology: true,
+      }
+    );
   }
 }
 
